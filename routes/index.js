49edba955dd123dcb25512c649f3a7d7d21a3c06
guired13 <red2006135@gmail.com>
2025-05-05 00:51:48 +0800

    Add MongoDB connection and update application title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'University Registration System' });
 });
 
 module.exports = router;
