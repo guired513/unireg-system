6f8d3ba3bc528173299152f6e7ff31afd063d64d
guired13 <red2006135@gmail.com>
2025-05-05 21:40:42 +0800

    restart from start

--- models/Enrollment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const enrollmentSchema = new mongoose.Schema({
-  student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  section: { type: mongoose.Schema.Types.ObjectId, ref: "Section" },
-});
-
-module.exports = mongoose.model("Enrollment", enrollmentSchema);
--- models/Program.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const programSchema = new mongoose.Schema({
-  name: String,
-  code: String, // e.g., BSIT
-});
-
-module.exports = mongoose.model("Program", programSchema);
